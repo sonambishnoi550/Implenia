d47c2e0e4289cdab42909765375ee3e4cbd3b852
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-02-20 09:32:22 +0530

    change padding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,17 +9,17 @@ const Hero = () => {
     return (
         <div
             id="home"
-            className="bg-center bg-cover hero-bg-mobile bg-no-repeat relative pb-20 overflow-hidden"
+            className="bg-center bg-cover hero-bg-mobile bg-no-repeat relative md:pb-20 pb-5 overflow-hidden"
             style={{ backgroundImage: `url(/assets/images/webp/hero-bg.webp)` }}
         >
             <Header />
             <img data-aos="fade-right"
-                className="absolute  md:top-[47%] top-[61%] pointer-events-none lg:w-[475px] lg:h-[402px] md:size-[350px] size-[170px]"
+                className="absolute  md:top-[53%] top-[56%] pointer-events-none lg:w-[475px] lg:h-[402px] md:size-[350px] size-[170px]"
                 src="/assets/images/webp/dustbin.webp"
                 alt="dustbin"
             />
             <div className="container max-w-[1140px] mx-auto">
-                <div className="flex flex-col max-xl:justify-center max-xl:items-center lg:pt-[350px] md:pt-[290px] pt-[145px] xl:pl-14 relative z-20">
+                <div className="flex flex-col max-xl:justify-center max-xl:items-center lg:pt-[350px] md:pt-[290px] pt-[67px] xl:pl-14 relative z-20">
                     <h2
                         className="font-bold max-sm:pt-5 text-customSm [text-shadow:0px_4px_4px_rgba(0,0,0,0.25)] text-white text-left"
                     >
