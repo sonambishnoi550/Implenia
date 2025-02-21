8a2996ffab259dc71eaf3dff9bd95ab3e4a07add
sonambishnoi550 <bishnoisonam079@gmail.com>
2025-02-21 10:53:15 +0530

    set layar

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,14 +9,14 @@ const Hero = () => {
     return (
         <div
             id="home"
-            className="bg-center bg-cover hero-bg-mobile bg-no-repeat relative pb-20 overflow-hidden"
+            className="bg-center bg-cover hero-bg-mobile bg-no-repeat relative pb-20 overflow-hidden max-sm:bg-black/20"
             style={{ backgroundImage: `url(/assets/images/webp/hero-bg.webp)` }}
         >
             <Header />
             <Image className="absolute  lg:top-[44%] md:top-[38%] top-[58%] pointer-events-none md:size-[410px] size-[170px]"
                 src="/assets/images/webp/dustbin.webp"
                 alt="dustbin" width={430} height={350} />
-            <div className="container max-w-[1140px] mx-auto bg-black/20">
+            <div className="container max-w-[1140px] mx-auto">
                 <div className="flex flex-col max-xl:justify-center max-xl:items-center lg:pt-[178px] md:pt-[120px] pt-[67px] xl:pl-6 relative z-20">
                     <p
                         className="font-bold max-sm:pt-5 text-custom-sm [text-shadow:0px_4px_4px_rgba(0,0,0,0.25)] text-white text-left"
